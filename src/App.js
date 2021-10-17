7d3ebc28066f13ca5c29933f996caae6478c1e25
nferdous2 <noorjahanferdous029@gmail.com>
2021-10-17 12:50:10 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Components/Header/Header';
+import Person from './Components/Person/Person';
+
+function App() {
+  return (
+    <div className="body-container">
+      <Header />
+      <Person></Person>
+
+    </div>
+  );
+}
+
+export default App;
